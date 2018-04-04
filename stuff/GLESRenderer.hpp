dd3a81b7c3b9165bb26f6aa4812af67de9d9e103
Rian Neufelt <RNeufelt@live.ca>
2018-04-03 20:57:12 -0700

    obj loader thing
    "IN THEORY"

--- stuff/GLESRenderer.hpp
+++ stuff/GLESRenderer.hpp
@@ -12,20 +12,16 @@
 #include <stdlib.h>
 
 #include <OpenGLES/ES3/gl.h>
-
-typedef struct
-{
-    GLfloat* position;
-    GLfloat* normals;
-    GLfloat* texCoords;
-} vertexStruct;
+#include <vector>
+#include <GLKit/GLKMathTypes.h>
 
 class GLESRenderer
 {
 public:
-    char *LoadShaderFile(const char *shaderFileName);
-    GLuint LoadShader(GLenum type, const char *shaderSrc);
-    GLuint LoadProgram(const char *vertShaderSrc, const char *fragShaderSrc);
+    char* LoadShaderFile(const char* shaderFileName);
+    GLuint LoadShader(GLenum type, const char* shaderSrc);
+    GLuint LoadProgram(const char* vertShaderSrc, const char* fragShaderSrc);
+    bool LoadOBJ(const char* path, std::vector<GLKVector3>& vertices, std::vector<GLKVector2>& uvs, std::vector<GLKVector3>& normals);
 
     int GenCube(float scale, float** vertices, float** normals, float** texCoords, int** indices);
     int GenQuad(float scale, float** vertices, float** normals, float** texCoords, int** indices);
